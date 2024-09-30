d605f3b7422a7c2480510a6293ec1cbe30ebe3da
yht0827 <yangheetae0827@naver.com>
2024-09-30 15:14:58 +0900

    Initial commit

---
+++ src/main/java/com/example/freelancerpoint/FreelancerPointApplication.java
@@ -0,0 +1,13 @@
+package com.example.freelancerpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancerPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelancerPointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/freelancerpoint/FreelancerPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freelancerpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancerPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
